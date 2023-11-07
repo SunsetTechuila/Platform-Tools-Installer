9dd500365e0ae073f6a65624824d2895f38926d2
Grigory <techuila.sunset@gmail.com>
2023-11-07 14:24:59 +0500

    refactor

--- Platform-Tools-Installer.ps1
+++ Platform-Tools-Installer.ps1
@@ -1,43 +1,48 @@
-#Requires -RunAsAdministrator
-#Requires -Version 5.1
+if ($Host.Version.Major -lt 5) {
+	Write-Host -Object 'PowerShell 5 or higher is required.' -ForegroundColor 'Red'
+	Start-Sleep -Seconds 2
+	Exit
+}
 
 $ErrorActionPreference = 'Stop'
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Add-Type -AssemblyName System.Windows.Forms
-$folderDialog = New-Object -TypeName System.Windows.Forms.FolderBrowserDialog
+Add-Type -AssemblyName 'System.Windows.Forms'
+$folderDialog = New-Object -TypeName 'System.Windows.Forms.FolderBrowserDialog'
 
-Write-Host
-Write-Host -Object 'Downloading Platform Tools...'
-Write-Host
-Write-Host -Object 'Do not switch to another window!'
-
-Invoke-WebRequest -Uri https://dl.google.com/android/repository/platform-tools-latest-windows.zip -OutFile $env:Temp\platform-tools.zip
+Write-Verbose -Message 'Downloading Platform Tools...' -Verbose
+$Parameters = @{
+	Uri             = 'https://dl.google.com/android/repository/platform-tools-latest-windows.zip'
+	OutFile         = "$env:Temp\platform-tools.zip"
+	UseBasicParsing = $true
+}
+Invoke-WebRequest @Parameters
 
 Write-Host
 Write-Host -Object 'Please choose a folder to install Platform Tools'
 
-if ( $folderDialog.ShowDialog() -eq 'OK' ) {
+if ($folderDialog.ShowDialog() -eq 'OK') {
 	Write-Host
-	Write-Host -Object 'Unpacking Platform Tools...'
-	
-	Expand-Archive -Path $env:Temp\platform-tools.zip -DestinationPath $folderDialog.SelectedPath -Force
+	Write-Verbose -Message 'Unpacking Platform Tools...' -Verbose
+	$Parameters = @{
+		Path        = "$env:Temp\platform-tools.zip"
+		Destination = $folderDialog.SelectedPath
+		Force       = $true
+	}
+	Expand-Archive @Parameters
 	
 	$platformToolsPath = "$($folderDialog.SelectedPath)\platform-tools"
-	
 	if ($env:Path -notlike "*$platformToolsPath*") {
 		[Environment]::SetEnvironmentVariable('Path', "$env:Path`;$platformToolsPath", 'User')
 	}
 	
-	Remove-Item -Path $env:Temp\platform-tools.zip -Force
+	Remove-Item -Path $Parameters.Path -Force
 	
 	Write-Host
-	Write-Host -Object 'Platform Tools has been successfully installed!' -ForegroundColor Green
-	
+	Write-Host -Object 'Platform Tools has been successfully installed!' -ForegroundColor 'Green'
 	Start-Sleep -Seconds 2
 }
 else {
 	Write-Host
-	Write-Host -Object 'Folder has not been selected!' -ForegroundColor Red
-	
+	Write-Host -Object 'Folder has not been selected!' -ForegroundColor 'Red'
 	Start-Sleep -Seconds 2
 }
--- README.md
+++ README.md
@@ -3,23 +3,32 @@
 ![pti](https://user-images.githubusercontent.com/115353812/221406600-16fb339b-d5de-4359-808e-7c8d31876c2a.png)
 
 ## How to (Windows)
-Run PowerShell as Administrator, paste this commands and press Enter.
+
+Run PowerShell as Administrator, paste these commands and then press Enter.
+
 ```powershell
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-IWR -UseB 'https://raw.githubusercontent.com/SunsetTechuila/Platform-Tools-Installer/main/Platform-Tools-Installer.ps1' | IEX
+iwr 'https://raw.githubusercontent.com/SunsetTechuila/Platform-Tools-Installer/main/Platform-Tools-Installer.ps1' -useb | iex
 ```
+
 You can also update Platform Tools in this way.
-### Requirements
+
+## Requirements (Windows)
+
 - Windows 7 or higher
-- PowerShell 5.1 or higher
-- Last Windows updates installed
+- PowerShell 5 or higher
 
 ## How to (Linux)
+
 Open a console, paste this command and press Enter
+
 ```shell
 sudo wget -qO- https://raw.githubusercontent.com/SunsetTechuila/Platform-Tools-Installer/main/Platform-Tools-Installer.sh | bash
 ```
+
 You can also update Platform Tools in this way.
-### Requirements
+
+## Requirements (Linux)
+
 - Bash
 - Unzip package
